4d6115219d4c95dc8cdf74960b31fe56301f17be
Bill <bunnkywhat@gmail.com>
2023-04-14 17:44:21 -0400

    Update src/main/java/dev/sefiraat/cultivation/implementation/slimefun/CultivationStacks.java
    Co-authored-by: Sefiraat <miakis@me.com>

--- src/main/java/dev/sefiraat/cultivation/implementation/slimefun/CultivationStacks.java
+++ src/main/java/dev/sefiraat/cultivation/implementation/slimefun/CultivationStacks.java
@@ -3503,8 +3503,8 @@ public final class CultivationStacks {
         "A healthy choice.",
         "",
         Theme.CLICK_INFO.asTitle("Healing", "Full"),
-        Theme.CLICK_INFO.asTitle("Effect 1", "Health Boost + 5"),
-        Theme.CLICK_INFO.asTitle("Effect 2", "Speed + 4")
+        Theme.CLICK_INFO.asTitle("Effect 1", "Health Boost + 2"),
+        Theme.CLICK_INFO.asTitle("Effect 2", "Speed + 1")
     );
 
     public static final SlimefunItemStack RAMEN_CHICKEN_HEARTY = Theme.themedSlimefunItemStack(
